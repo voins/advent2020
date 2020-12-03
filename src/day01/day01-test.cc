bb92c30d65240700274af2fd7867f504b0c81ffa
Alexey Voinov <alexey.v.voinov@gmail.com>
2020-12-03 17:31:07 +0100

    straightforward solution for three numbers

--- src/day01/day01-test.cc
+++ src/day01/day01-test.cc
@@ -5,15 +5,28 @@ template <typename I>
 auto process(I first, I last) {
     if (first == last)
         return 0;
-    auto part = *first;
+
+    auto part1 = *first;
 
     first++;
     if (first == last)
         return 0;
 
-    if (part + *first == 2020)
-        return part * *first;
-    
+    if (part1 + *first == 2020)
+        return part1 * *first;
+
+    auto part2 = *first;
+
+    first++;
+    if (first == last)
+        return 0;
+
+    if (part1 + *first == 2020)
+        return part1 * *first;
+   
+    if (part2 + *first == 2020)
+        return part2 * *first;
+
     return 0;
 }
 
@@ -42,6 +55,18 @@ auto main() -> int {
         auto input = std::vector<int>{2, 2019};
         auto result = process(input.begin(), input.end());
         expect(that % result == 0);
+    };
+
+    "returns product for input of three numbers, bad number second"_test = [] {
+        auto input = std::vector<int>{2, 2019, 2018};
+        auto result = process(input.begin(), input.end());
+        expect(that % result == 4036);
+    };
+
+    "returns product for input of three numbersbad number first"_test = [] {
+        auto input = std::vector<int>{2019, 2, 2018};
+        auto result = process(input.begin(), input.end());
+        expect(that % result == 4036);
     };
     return 0;
 }
