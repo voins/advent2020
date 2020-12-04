4ac23056fa17aa49c14a116e6e0b7cc96fe80266
Alexey Voinov <alexey.v.voinov@gmail.com>
2020-12-04 20:51:48 +0100

    straightforward solution for 3 number input

--- src/day01/day01-test3.cc
+++ src/day01/day01-test3.cc
@@ -3,7 +3,25 @@
 #include <boost/ut.hpp>
 
 template <typename I>
-auto process3(I, I) {
+auto process3(I first, I last) {
+    if (first == last)
+        return 0;
+
+    auto part_a = *first;
+    ++first;
+
+    if (first == last)
+        return 0;
+
+    auto part_b = *first;
+    ++first;
+
+    if (first == last)
+        return 0;
+
+    if (part_a + part_b + *first == 2020)
+        return part_a * part_b * *first;
+    
     return 0;
 }
 
@@ -24,6 +42,18 @@ auto main() -> int {
 
     "returns 0 for input of two numbers"_test = [] {
         auto input = std::vector<int>{1, 2019};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 0);
+    };
+
+    "returns product for input of three numbers summing to 2020"_test = [] {
+        auto input = std::vector<int>{2, 3, 2015};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 12090);
+    };
+
+    "returns 0 for input of three numbers not summing to 2020"_test = [] {
+        auto input = std::vector<int>{2, 3, 2014};
         auto result = process3(input.begin(), input.end());
         expect(that % result == 0);
     };
