42369ecfb738af1a9ab3b8ee599742b0dac82c21
Alexey Voinov <alexey.v.voinov@gmail.com>
2020-12-04 20:37:31 +0100

    day 1: Find the solution for the second task

---
+++ src/day01/day01-test3.cc
@@ -0,0 +1,77 @@
+#include <vector>
+#include <set>
+#include "process.hh"
+#include <boost/ut.hpp>
+
+template <typename I>
+auto process3(I first, I last) {
+    while (first != last) {
+        auto part_a = *first;
+        auto second = ++first;
+        auto parts = std::set<int>{};
+
+        while (second != last) {
+            if (parts.count(2020 - part_a - *second))
+                return (2020 - part_a - *second) * part_a * *second;
+
+            parts.insert(*second);
+            second++;
+        }
+    }
+
+    return 0;
+}
+
+auto main() -> int {
+    using namespace boost::ut;
+
+    "returns 0 for empty input"_test = [] {
+        auto input = std::vector<int>{};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 0);
+    };
+
+    "returns 0 for input of one number"_test = [] {
+        auto input = std::vector<int>{2020};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 0);
+    };
+
+    "returns 0 for input of two numbers"_test = [] {
+        auto input = std::vector<int>{1, 2019};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 0);
+    };
+
+    "returns product for input of three numbers summing to 2020"_test = [] {
+        auto input = std::vector<int>{2, 3, 2015};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 12090);
+    };
+
+    "returns 0 for input of three numbers not summing to 2020"_test = [] {
+        auto input = std::vector<int>{2, 3, 2014};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 0);
+    };
+
+    "returns product for input of four numbers, bad number third"_test = [] {
+        auto input = std::vector<int>{2, 3, 2000, 2015};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 12090);
+    };
+
+    "returns product for input of four numbers, bad number second"_test = [] {
+        auto input = std::vector<int>{2, 2000, 3, 2015};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 12090);
+    };
+
+    "returns product for input of four numbers, bad number first"_test = [] {
+        auto input = std::vector<int>{2000, 2, 3, 2015};
+        auto result = process3(input.begin(), input.end());
+        expect(that % result == 12090);
+    };
+
+    return 0;
+}
